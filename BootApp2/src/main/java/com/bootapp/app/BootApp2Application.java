fea54b7d1acc9242c6f49347541e6a37e5319b53
91801 <91801@LAPTOP-BKAHRGBP>
2019-11-11 20:08:49 +0530

    Initial commit
    Initial Commit

---
+++ BootApp2/src/main/java/com/bootapp/app/BootApp2Application.java
@@ -0,0 +1,13 @@
+package com.bootapp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApp2Application.class, args);
+	}
+
+}
